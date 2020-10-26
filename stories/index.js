3bb6dc4fc0644a49382eda42069967ff3ac4fe88
Sean Knox <seanknox0@gmail.com>
2020-10-26 18:24:58 +0000

    Movie details page components

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,4 @@
+import MovieDetails from "../src/components/movieDetails";
 import MovieList from "../src/components/movieList";
 import MoviesHeader from "../src/components/headerMovieList";
 import FilterControls from "../src/components/filterControls";
@@ -107,3 +108,7 @@ storiesOf("Home Page/MovieList", module)
     const movies= [sample, sample, sample, sample, sample]
     return <MovieList movies={movies} />
 });
+
+storiesOf("Movie Details Page/MovieDetails", module).add("default", () => (
+  <MovieDetails movie={sample} />
+));
