4b9739dbd7ebf435676610042ec29f89ec3cf583
Sean Knox <seanknox0@gmail.com>
2020-10-30 14:18:37 +0000

    Show movie review excerpts; Refactor movie page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -4,7 +4,7 @@ export const getMovies = () => {
     )
       .then(res => res.json())
       .then(json => json.results);
-  };
+};
   
   export const getMovie = id => {
     return fetch(
@@ -20,4 +20,12 @@ export const getMovies = () => {
     )
       .then(res => res.json())
       .then(json => json.genres);
+  };
+
+  export const getMovieReviews = id => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    )
+      .then(res => res.json())
+      .then(json => json.results);
   };
