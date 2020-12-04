3fcfb0674666daf150707e34dd99eadad5a67eef
Sean Knox <seanknox0@gmail.com>
2020-12-04 18:48:47 +0000

    UI updated

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -8,30 +8,12 @@ const SiteHeader = () => {
   return (
     <nav className="navbar  navbar-light fixed-top  bg-dark ">
       <nav className="navbar-brand text-white">
-        <Link className=" text-white" to="/">
-          Home
+        <Link className="nav-link text-white" to="/">
+          Movies Central DB
         </Link>
       </nav>
-      <FontAwesomeIcon
-        className="navbar-text text-light"
-        icon={["fas", "play"]}
-        size="3x"
-      />
-      <span className="navbar-text text-light">
-        Movies Fan App!
-      </span>
-      <FontAwesomeIcon
-        className="navbar-text text-light"
-        icon={["fas", "tv"]}
-        size="3x"
-      />
       <nav className="navbar navbar-expand ">
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link text-white" to="/">
-              All Movies
-            </Link>
-          </li>
           <li className="nav-item">
             <Link className="nav-link text-white" to="/movies/popular">
               Popular
@@ -59,6 +41,19 @@ const SiteHeader = () => {
           </li>
         </ul>
       </nav>
+      <FontAwesomeIcon
+        className="navbar-text text-light"
+        icon={["fas", "play"]}
+        size="3x"
+      />
+      <span className="navbar-text text-light">
+        View Movies and People in the Movie Industry!
+      </span>
+      <FontAwesomeIcon
+        className="navbar-text text-light"
+        icon={["fas", "tv"]}
+        size="3x"
+      />
     </nav>
   );
 };
